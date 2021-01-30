ddd2774398090a7d0166c45ed834da89bae599d1
nanchaoke <isEaston@foxmail.com>
2021-01-30 14:14:05 +0800

    调整细节错误

--- themes/icarus/layout/common/article.jsx
+++ themes/icarus/layout/common/article.jsx
@@ -30,7 +30,7 @@ module.exports = class extends Component {
             <div class={index?'card card-index':'card card-context'}>
                 {/* Thumbnail */}
                 {has_thumbnail(page) ? <div class={`card-image ${index ? 'card-image-index' : 'card-image-context'}`}>
-                    <a class="image is-7by3 image-pic" href={index ? url_for(page.link || page.path) : `javascript:;`} target="_blank">
+                    <a class="image is-7by3 image-pic" href={index ? url_for(page.link || page.path) : `javascript:;`} target={index ? '_blank' : '_self'}>
                         {index ? <span class="light-move light-hide"></span> : null}
                         <img class="thumbnail" src={get_thumbnail(page)} alt={page.title || get_thumbnail(page)} />
                     </a>
