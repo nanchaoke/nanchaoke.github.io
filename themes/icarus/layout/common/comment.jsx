85118fefc00e34066ec45ff70842b9fec8685d32
nanchaoke <iseaston@foxmail.com>
2021-07-16 21:07:46 +0800

    更换favicon

--- themes/icarus/layout/common/comment.jsx
+++ themes/icarus/layout/common/comment.jsx
@@ -20,7 +20,7 @@ module.exports = class extends Component {
                         Comment = Comment.Cacheable ? Comment.Cacheable : Comment;
                         return <Comment config={config} page={page} helper={helper} comment={comment} />;
                     } catch (e) {
-                        logger.w(`Icarus cannot load comment "${comment.type}"`);
+                        logger.w(`无法加载评论 "${comment.type}"`);
                         return null;
                     }
                 })()}
